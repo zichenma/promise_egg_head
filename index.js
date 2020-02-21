711b94c294a8a456567bbf13238b66549f76a5c0
zichenma <zma@liaisionedu.com>
2020-02-21 12:43:18 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const API_URL = 'https://starwars.egghead.training/';
+const output = document.getElementById("output");
+output.innerText = "Loading...";
+
+fetch(API_URL + 'films')
+.then(function(res) {
+    return res.json();
+}).then(function(films) {
+    output.innerText = getFilmTitles(films);
+})
+
+function getFilmTitles(films) {
+    return films
+    .sort((a, b) => a.episode_id - b.episode_id)
+    .map(film => `${film.episode_id}. ${film.title}`)
+    .join('\n');
+
+}
+
+
+
+
+
